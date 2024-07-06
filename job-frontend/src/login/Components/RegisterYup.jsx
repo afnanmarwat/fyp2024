f7e2e5e4660f14b663da91c1c0554fe7207cf7b7
Tayyab16203 <rehmantayyab0340@gmail.com>
2024-07-06 10:57:11 +0500

    pull a

--- job-frontend/src/login/Components/RegisterYup.jsx
+++ job-frontend/src/login/Components/RegisterYup.jsx
@@ -33,26 +33,26 @@ const Register = (props) => {
   //   role: "User",
   // };
   const initialFormData = {
-    role: '',
-    name: '',
-    email: '',
-    password: '',
-    mobile: '',
-    age: '',
-    gender: '',
-    qualification: '',
-    experience: ''
+    role: "",
+    name: "",
+    email: "",
+    password: "",
+    mobile: "",
+    age: "",
+    gender: "",
+    qualification: "",
+    experience: "",
   };
 
   const [formData, setFormData] = useState(initialFormData);
-  console.log(formData)
+  console.log(formData);
   const handleFormChange = (e) => {
     const { name, value, type, checked } = e.target;
-    const newValue = type === 'checkbox' ? checked : value;
+    const newValue = type === "checkbox" ? checked : value;
 
-    setFormData(prevFormData => ({
+    setFormData((prevFormData) => ({
       ...prevFormData,
-      [name]: newValue
+      [name]: newValue,
     }));
   };
   const formSubmitHandler = (event) => {
@@ -91,16 +91,16 @@ const Register = (props) => {
   return (
     <React.Fragment>
       {/* <Header /> */}
-      <section className="h-[150vh] md:h-[130vh] 2xl:h-[100vh] bg-[rgba(243,247,250,255)] md:py-4 px-1 grid sm:place-items-center  overflow-hidden">
+      <section className="h-full bg-[rgba(243,247,250,255)] py-4 px-1 grid place-items-center overflow-hidden">
         <div className="relative">
           {/* large circle design */}
-          <div className="hidden sm:block bg-[rgba(28,220,199,255)] absolute top-[-90px] left-[300px] z-10 shadow-lg  rounded-full w-[300px] h-[300px]"></div>
-          <div className="hidden sm:block bg-[rgba(152,145,212,255)] absolute top-[450px] left-[-120px] z-10 shadow-lg  rounded-full w-[300px] h-[300px]"></div>
+          <div className="hidden md:block bg-[rgba(28,220,199,255)] absolute top-[-90px] left-[300px] z-10 shadow-lg  rounded-full w-[300px] h-[300px]"></div>
+          <div className="hidden md:block bg-[rgba(152,145,212,255)] absolute top-[450px] left-[-120px] z-10 shadow-lg  rounded-full w-[300px] h-[300px]"></div>
           {/* small circle design */}
-          <div className="hidden sm:block bg-[#1de6b5] absolute top-[400px] left-[-180px] z-10 shadow-lg  rounded-full w-[20px] h-[20px]"></div>
-          <div className="hidden sm:block bg-[#03a8ef] absolute top-[250px] left-[600px] z-10 shadow-lg  rounded-full w-[20px] h-[20px]"></div>
+          <div className="hidden md:block bg-[#1de6b5] absolute top-[400px] left-[-180px] z-10 shadow-lg  rounded-full w-[20px] h-[20px]"></div>
+          <div className="hidden md:block bg-[#03a8ef] absolute top-[250px] left-[600px] z-10 shadow-lg  rounded-full w-[20px] h-[20px]"></div>
           {showSpinner && <SpinnerComponent />}
-          <div className="flex flex-col gap-4 relative z-20 bg-white xs:px-3 xs:py-2 sm:px-5 sm:py-3 shadow-lg md:min-w-[500px] min-w-[250px] rounded">
+          <div className="flex flex-col gap-4 relative z-20 bg-white px-5 py-3 shadow-lg min-w-[500px] rounded">
             <h1 className="flex items-center justify-center text-xl font-bold ">
               Sign-Up
             </h1>
@@ -108,29 +108,37 @@ const Register = (props) => {
               {/* form */}
               <Form className="flex flex-col gap-3">
                 {/* Role */}
-                <div className="flex flex-col gap-1" >
+                <div className="flex flex-col gap-1">
                   <label htmlFor="role">Role</label>
-                  <FormSelect name="role" id="role" label="Role">
+                  <FormSelect
+                    name="role"
+                    id="role"
+                    label="Role"
+                    value={formData.role}
+                    onChange={handleFormChange}
+                  >
                     <option value="User">User</option>
                     <option value="Job Provider">Job Provider</option>
                   </FormSelect>
                 </div>
                 {/* name */}
-                <div className="flex flex-col justify-center ">
+                <div className="flex flex-col justify-center">
                   <Form.Control
                     id="name"
                     name="name"
-                    mandatory={"true"}
                     placeholder="Full Name"
+                    value={formData.name}
+                    onChange={handleFormChange}
                   />
                 </div>
                 {/* email */}
-                <div className="flex flex-col ">
+                <div className="flex flex-col">
                   <Form.Control
                     id="email"
                     name="email"
-                    mandatory={"true"}
                     placeholder="Enter Email"
+                    value={formData.email}
+                    onChange={handleFormChange}
                   />
                 </div>
                 {/* password */}
@@ -139,8 +147,9 @@ const Register = (props) => {
                     id="password"
                     type="password"
                     name="password"
-                    mandatory={"true"}
                     placeholder="Password"
+                    value={formData.password}
+                    onChange={handleFormChange}
                   />
                 </div>
                 {/* mobile number */}
@@ -148,8 +157,9 @@ const Register = (props) => {
                   <Form.Control
                     id="mobile"
                     name="mobile"
-                    mandatory={"true"}
                     placeholder="Mobile No"
+                    value={formData.mobile}
+                    onChange={handleFormChange}
                   />
                 </div>
                 {/* age and gender */}
@@ -160,15 +170,14 @@ const Register = (props) => {
                       name="age"
                       id="age"
                       type="number"
-                      mandatory={"true"}
                       placeholder="Age"
+                      value={formData.age}
+                      onChange={handleFormChange}
                     />
                   </div>
                   {/* Gender */}
                   <div className="w-[50%]">
-                    <Form.Label>
-                      Gender
-                    </Form.Label>
+                    <Form.Label>Gender</Form.Label>
                     <div className="flex gap-3">
                       {/* male */}
                       <div className="flex gap-1">
@@ -177,6 +186,8 @@ const Register = (props) => {
                           value="Male"
                           name="gender"
                           id="Male"
+                          checked={formData.gender === "Male"}
+                          onChange={handleFormChange}
                         />
                         <label htmlFor="Male">Male</label>
                       </div>
@@ -187,13 +198,12 @@ const Register = (props) => {
                           value="Female"
                           name="gender"
                           id="Female"
+                          checked={formData.gender === "Female"}
+                          onChange={handleFormChange}
                         />
                         <label htmlFor="Female">Female</label>
                       </div>
                     </div>
-                    {formik.errors.gender && (
-                      <div className="error">{formik.errors.gender}</div>
-                    )}
                   </div>
                 </div>
                 {/* Qualification and Experience */}
@@ -205,7 +215,8 @@ const Register = (props) => {
                       name="qualification"
                       id="qualification"
                       label="Qualification"
-                      mandatory={"true"}
+                      value={formData.qualification}
+                      onChange={handleFormChange}
                     >
                       <option value="">Select</option>
                       <option value="Post Graduate">Post Graduate</option>
@@ -221,6 +232,8 @@ const Register = (props) => {
                       name="experience"
                       id="experience"
                       label="Experience"
+                      value={formData.experience}
+                      onChange={handleFormChange}
                     >
                       <option value="">Select</option>
                       <option value="0-2">0-2</option>
@@ -232,22 +245,22 @@ const Register = (props) => {
                 </div>
                 {/* sign-up and login button */}
                 <div className="flex flex-col gap-3 justify-between mt-3">
-                  <Button variant="outline-success" type="submit" className="w-full">
+                  <Button
+                    variant="outline-success"
+                    type="submit"
+                    className="w-full"
+                    onClick={formSubmitHandler}
+                  >
                     Register
                   </Button>
                   <Link to="/Login" className="flex justify-end">
-                    <Button
-                      variant="outline-primary"
-                      type="submit"
-                    >
+                    <Button variant="outline-primary" type="submit">
                       Back to Login
                     </Button>
                   </Link>
                 </div>
               </Form>
             </div>
-
-
           </div>
         </div>
       </section>
