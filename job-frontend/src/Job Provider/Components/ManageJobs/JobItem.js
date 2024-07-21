60f406d6a3e5f3d5dea66dd05222db0ef0e9b723
Tayyab16203 <rehmantayyab0340@gmail.com>
2024-07-21 09:49:16 +0500

    reponsive a jobpage and add focus to links  in sidebar

--- job-frontend/src/Job Provider/Components/ManageJobs/JobItem.js
+++ job-frontend/src/Job Provider/Components/ManageJobs/JobItem.js
@@ -22,33 +22,31 @@ const JobItem = (props) => {
     props.onDelete(props.jobInfo._id);
   };
   return (
-    <tr className={classes.row} key={props.jobInfo._id}>
-      {/* <td>{props.jobInfo._id}</td>
-      <td>{props.jobInfo.providerId}</td> */}
-      <td>{props.jobInfo.title}</td>
-      <td>{props.jobInfo.description}</td>
-      <td>{props.jobInfo.category}</td>
-      <td>{props.jobInfo.startDate}</td>
-      <td>{props.jobInfo.endDate}</td>
-      <td className={classes.actions}>
+    <tr className="text-[#808080] hover:bg-[#0000001f] border-2" key={props.jobInfo._id}>
+      <td className="px-4 py-3 whitespace-nowrap">{props.jobInfo.title}</td>
+      <td className="px-4 py-3 whitespace-nowrap">{props.jobInfo.description}</td>
+      <td className="px-4 py-3 whitespace-nowrap">{props.jobInfo.category}</td>
+      <td className="px-4 py-3 whitespace-nowrap">{props.jobInfo.startDate}</td>
+      <td className="px-4 py-3 whitespace-nowrap">{props.jobInfo.endDate}</td>
+      <td className="flex gap-3 px-4 py-3 ">
         <button
-          className={`${classes.edit} ${classes.button}`}
+          className="w-[100px] flex gap-2 items-center border-2 hover:border-green-700 hover:bg-[#18f97d39] hover:text-green-700  p-2 rounded-lg transition-all ease-in-out text-lg"
           onClick={editButtonHandler}
         >
           <span>
-            <i className="bi bi-pencil-fill"></i>
+            <i className="bi bi-pencil-fill text-md"></i>
           </span>
           <span>Edit</span>
         </button>
-        <Button
-          className={`${classes.delete} ${classes.button}`}
+        <button
+          className="w-[100px] flex gap-2 items-center border-2 hover:border-red-700 hover:bg-[#f918182d] hover:text-red-700  p-2 rounded-lg transition-all ease-in-out text-lg"
           onClick={deleteButtonHandler}
         >
           <span>
             <i className="bi bi-trash"></i>
           </span>
           <span>Delete</span>
-        </Button>
+        </button>
       </td>
     </tr>
   );
