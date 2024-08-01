eb672dc426f59fa3f4ba5975d04a5acd00d5925d
Tayyab16203 <rehmantayyab0340@gmail.com>
2024-08-01 23:22:50 +0500

    user and profile tab responsive

--- job-frontend/src/components/profile/Profile.jsx
+++ job-frontend/src/components/profile/Profile.jsx
@@ -1,33 +1,54 @@
 import jwtDecode from 'jwt-decode';
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { FaFacebook, FaLinkedin } from 'react-icons/fa6'
-// import Config from "../../config/Config.json";
-// import axios from "axios";
+import Config from "../../config/Config.json";
+import axios from "axios";
 
-// let Data = [];
+let Data = [];
 const Profile = () => {
     
-    // const [ profile , setProfile ] = useState([])
+    const [ userProfile , setUserProfile ] = useState([]);
+    const [ proProfile , setProProfile ] = useState([]);
+
     const authToken = localStorage.getItem("token");
     const redAuthToken = jwtDecode(authToken);
 
-    // useEffect(() => {
-    //     axios
-    //     .get(`${Config.SERVER_URL + "user/profile"}`, {
-    //         headers: {
-    //           Authorization: "Bearer " + localStorage.getItem("token"),
-    //         },
-    //       })
-    //       .then((response) => {
-    //         Data = response.data.profile;
-    //         setProfile(response.data.profile);
-    //         console.log(response.data.profile);
-    //       })
-    //       .catch((err) => {
-    //         console.log(err);
-    //       });
-    //   }, []);
+    // user Profile
+    useEffect(() => {
+        axios
+        .get(`${Config.SERVER_URL + "user/profile"}`, {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          })
+          .then((response) => {
+            Data = response.data.profile;
+            setUserProfile(response.data.profile);
+            console.log("userProfile : ",response.data.profile);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, []);
 
+    //   provider Profile
+    useEffect(() => {
+        axios
+        .get(`${Config.SERVER_URL + "provider/profile"}`, {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          })
+          .then((response) => {
+            Data = response.data.profile;
+            setProProfile(response.data.profile);
+            console.log("ProProfile : ",response.data.profile);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, []);
+    
     return (
         <>
             {redAuthToken.role === "JobProvider" && (
@@ -38,11 +59,11 @@ const Profile = () => {
                         <div className='md:max-w-[30%] flex flex-col gap-2 p-1 justify-center items-center'>
                             {/* img */}
                             <div className='w-[150px] h-[150px] border-3 border-[#2085cf] overflow-hidden rounded-full'>
-                                <img src="./images/profile/my_pic.png" alt="" />
+                                {proProfile.profilePic}
                             </div>
                             {/* name */}
                             <div>
-                                <h4 className='text-xl font-bold text-black'>Tayyab Ur Rehman</h4>
+                                <h4 className='text-xl font-bold text-black'>{proProfile.company}</h4>
                             </div>
                             {/* update and message */}
                             <div className='flex gap-2'>
@@ -51,9 +72,9 @@ const Profile = () => {
                             </div>
                         </div>
                         {/* Bio part */}
-                        <div className='md:min-w-[70%] py-4 px-2'>
+                        <div className='md:min-w-[70%] self-start py-4 px-2'>
                             <p className='text-gray-400 font-semibold py-2'>BIO</p>
-                            <p className='text-sm text-[#545454]'>A company with a Lot of Facilities. Work on the Websites and Mobile Application and also work on Graphic Designing. With Skills having Html, CSS, JavaScript, JS Libraries Like Reactjs Angularjs on a web development site. And a React Native and Flutter is used for Mobile Application. For Graphic Designing we used Photshop and illustrator. </p>
+                            <p className='text-sm text-[#545454]'>{proProfile.bio}</p>
                         </div>
                     </div>
                     {/* Location URls and Social Media Links div */}
@@ -65,8 +86,8 @@ const Profile = () => {
                         </div>
                         {/* Location part */}
                         <div className='pb-3 border-b-2 mb-4'>
-                            <p className='text-gray-400 text-md font-semibold py-1'>URL</p>
-                            <p className='text-lg'>www.monoSoft.com</p>
+                            <p className='text-gray-400 text-md font-semibold py-1'>Email</p>
+                            <p className='text-lg'>{proProfile.email}</p>
                         </div>
                         {/* Location part */}
                         <div className='pb-3'>
@@ -92,7 +113,7 @@ const Profile = () => {
                             </div>
                             {/* name */}
                             <div>
-                                <h4 className='text-xl font-bold text-black'>Rehman Khan</h4>
+                                <h4 className='text-xl font-bold text-black'>{userProfile.name}</h4>
                             </div>
                             {/* update and message */}
                             <div className='flex gap-2'>
@@ -102,23 +123,28 @@ const Profile = () => {
                         </div>
                         {/* Bio part */}
                         <div className='md:min-w-[70%] py-4 px-2'>
-                            <p className='text-gray-400 font-semibold py-2'>BIO</p>
+                            <p className='text-gray-400 font-semibold py-2'>About Us</p>
                             <p className='text-sm text-[#545454]'>A company with a Lot of Facilities. Work on the Websites and Mobile Application and also work on Graphic Designing. With Skills having Html, CSS, JavaScript, JS Libraries Like Reactjs Angularjs on a web development site. And a React Native and Flutter is used for Mobile Application. For Graphic Designing we used Photshop and illustrator. </p>
                         </div>
                     </div>
-                    {/* Location URls and Social Media Links div */}
+                    {/* Location Emails and Social Media Links div */}
                     <div className='p-3 mt-3 rounded'>
                         {/* Location part */}
                         <div className='pb-3 border-b-2 mb-4'>
                             <p className='text-gray-400 text-md font-semibold py-1'>Location</p>
                             <p className='text-lg'>Islamabad, Model Town, Street 509</p>
                         </div>
-                        {/* Location part */}
+                        {/* Email part */}
                         <div className='pb-3 border-b-2 mb-4'>
-                            <p className='text-gray-400 text-md font-semibold py-1'>URL</p>
-                            <p className='text-lg'>www.monoSoft.com</p>
+                            <p className='text-gray-400 text-md font-semibold py-1'>Email</p>
+                            <p className='text-lg'>{userProfile.email}</p>
                         </div>
-                        {/* Location part */}
+                        {/* Contact Number part */}
+                        <div className='pb-3 border-b-2 mb-4'>
+                            <p className='text-gray-400 text-md font-semibold py-1'>Contact us</p>
+                            <p className='text-lg'>0{userProfile.mobile}</p>
+                        </div>
+                        {/* Social media part */}
                         <div className='pb-3'>
                             <p className='text-gray-400 text-md font-semibold py-2'>Social media links</p>
                             <div className='flex items-center gap-2 text-[30px]'>
